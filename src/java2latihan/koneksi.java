0e6603da0545f47ded34c792c4650e2b1a2fdde7
User <User@THINKPAD-X230>
2020-04-09 15:08:06 +0700

    Latihan Java 2

---
+++ src/java2latihan/koneksi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class koneksi {
+public Connection bukakoneksi(){
+Connection con=null;
+try{
+Class.forName("com.mysql.jdbc.Driver");
+con=(Connection)DriverManager.getConnection("jdbc:mysql://localhost/java2db","root","");
+System.out.println("berhasil");
+return con;
+}
+catch (Exception e){
+System.out.println("gagal");
+return null;
+}
+}
+public static void main (String args[]){
+new koneksi().bukakoneksi();
+}
+}
